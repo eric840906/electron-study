5a2137b36dfce5a7883bec2b5a29b4271cacb45a
eric840906 <ecericchiu1@gmail.com>
2023-01-12 17:14:10 +0800

    常數改名

---
+++ src/main/constants.ts
@@ -0,0 +1,19 @@
+export default {
+  formats: {
+    sizeArr: [
+      '1920x1080',
+      '1280x720',
+      '960x540',
+      '854x480',
+      '640x360',
+      '426x240',
+      '320x180'
+    ],
+    fpsArr: [8, 12, 15, 23.976, 24, 25, 29.97, 30, 50, 59.94, 60, 120],
+    imageFormatArr: ['webp', 'png', 'jpg']
+  },
+  event_keys: {
+    GET_INPUT_VIDEO: 'GET_INPUT_VIDEO',
+    TEST: 'TEST'
+  }
+}
